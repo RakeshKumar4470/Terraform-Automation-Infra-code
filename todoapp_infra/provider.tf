fcebaf6f6948a3ed75caff131125f94215ba6eca
RakeshKumar4470 <rk42942@gmail.com>
2025-09-28 17:08:51 +0530

    Terraform Automation Infra Code

---
+++ todoapp_infra/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.32.0"
+    }
+  }
+  backend "azurerm" {
+    RG_name              = "rg"
+    storage_Account_name = "rakshafurr1"
+    location             = "eastus"
+    container            = "pipeline"
+    key                  = " terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3805d418-505b-4d26-9254-6523d1b472cb"
+}
